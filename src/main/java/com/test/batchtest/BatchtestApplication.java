5c305daa1d30b3193063be79982d9a337dc3a644
ihebzouaghi <ihebzouaghi10@gmail.com>
2025-06-19 01:16:59 +0200

    Klx Spring Batch test

---
+++ src/main/java/com/test/batchtest/BatchtestApplication.java
@@ -0,0 +1,13 @@
+package batchtest.src.main.java.com.test.batchtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchtestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/batchtest/BatchtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.batchtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
